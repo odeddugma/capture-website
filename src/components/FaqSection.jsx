d5a0aa4735cabc7fb58e7ae68d109723d142bbdd
Oded Dugma <odeddugma@gmail.com>
2022-01-05 20:07:57 +0200

    FAQ section

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+	return (
+		<div className="faq">
+			<h2>
+				Any Question <span>FAQ</span>
+			</h2>
+			<div className="question">
+				<h4>How Do I start?</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Temporibus, fugit.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>Daily Schedule</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Temporibus, fugit.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>Diferrent Payment Methods</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Temporibus, fugit.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>What products do you offer?</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Temporibus, fugit.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default FaqSection;
