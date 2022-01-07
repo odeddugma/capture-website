1714003f2bcbf73a4d1b481b73113c4b1fb8518f
Oded Dugma <odeddugma@gmail.com>
2022-01-07 22:53:02 +0200

    Styling the about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,34 +1,65 @@
 // Import Images
 import home1 from "../img/home1.png";
+// Style
+import styled from "styled-components";
 
 const AboutSection = () => {
 	return (
-		<div>
-			<div className="description">
+		<AboutSC>
+			<DescriptionSC>
 				<div className="title">
-					<div className="hide">
+					<HideSC>
 						<h2>We work to make</h2>
-					</div>
-					<div className="hide">
+					</HideSC>
+					<HideSC>
 						<h2>
 							your <span>dreams</span> come
 						</h2>
-					</div>
-					<div className="hide">
+					</HideSC>
+					<HideSC>
 						<h2>true.</h2>
-					</div>
+					</HideSC>
 				</div>
 				<p>
 					Contact us for any photograph or videography ideas that you have. We
 					have professionals with amazing skills to help you achieve it.
 				</p>
 				<button>Contact Us</button>
-			</div>
-			<div className="image">
+			</DescriptionSC>
+			<ImageSC>
 				<img src={home1} alt="guy with a camera" />
-			</div>
-		</div>
+			</ImageSC>
+		</AboutSC>
 	);
 };
+
+// Styled Components
+const AboutSC = styled.div`
+	min-height: 90vh;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 5rem 10rem;
+	color: white;
+`;
+const DescriptionSC = styled.div`
+	flex: 1;
+	padding-right: 5rem;
+	h2 {
+		font-weight: lighter;
+	}
+`;
+const ImageSC = styled.div`
+	flex: 1;
+	overflow: hidden;
+	img {
+		width: 100%;
+		height: 80vh;
+		object-fit: cover;
+	}
+`;
+const HideSC = styled.div`
+	overflow: hidden;
+`;
 
 export default AboutSection;
