46d4403cfb71b02da16fc399cd8caaae145a2b40
Oded Dugma <odeddugma@gmail.com>
2022-01-12 12:47:05 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@
 import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+
+// Router
+import { Route, Routes } from "react-router-dom";
 
 import Nav from "./components/Nav";
 
@@ -10,7 +15,26 @@ function App() {
 		<div className="App">
 			<GlobalStyle />
 			<Nav />
-			<AboutUs />
+			<Routes>
+				<Route path="/" element={<AboutUs />} />
+				<Route path="/work" element={<OurWork />} />
+				<Route path="/contact" element={<ContactUs />} />
+			</Routes>
+
+			{/*
+      The old way before Reacr Router v6
+      <Switch>
+        <Route path="/" exact>
+				  <AboutUs />
+			  </Route>
+			  <Route path="/work">
+				  <OurWork />
+			  </Route>
+			  <Route path="/contact">
+				  <ContactUs />
+			  </Route>
+      </Switch>
+        */}
 		</div>
 	);
 }
